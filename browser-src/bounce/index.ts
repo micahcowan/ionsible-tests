ac42d1684528caed3e77fb730e6b6fe44279a5a7
Micah Cowan <micah@addictivecode.org>
2016-05-18 01:56:21 -0700

    Ion -> ion. Faster ball. Add circle body.

--- browser-src/bounce/index.ts
+++ browser-src/bounce/index.ts
@@ -1,33 +1,34 @@
-import * as Ion from "ionsible";
+import * as ion from "ionsible";
 import { test } from "../common/test";
 
-let game = new Ion.Game;
+let game = new ion.Game;
 
-class Background extends Ion.Sprite {
+class Background extends ion.Sprite {
     draw(c : CanvasRenderingContext2D) : void {
         c.fillStyle = "silver";
         c.fillRect(0, 0, c.canvas.width, c.canvas.height);
     }
 }
 
-class Ball extends Ion.Sprite {
+class Ball extends ion.Sprite {
     radius : number = 30;
-    vel = Ion.veloc(-75, -75);
+    vel = ion.veloc(-175, -175);
+    body = new ion.body.Circle(this.radius);
     draw(c : CanvasRenderingContext2D) : void {
         c.arc(0, 0, this.radius, 0, 2 * Math.PI);
         c.fillStyle = "blue";
         c.fill();
     }
     behaviors = [
-        Ion.b.Momentum
-      , Ion.b.Bounded(
-            Ion.util.gameRect
-          , Ion.util.spriteBounce
+        ion.b.Momentum
+      , ion.b.Bounded(
+            ion.util.gameRect
+          , ion.util.spriteBounce
         )
     ]
-    constructor(game : Ion.Game) {
+    constructor(game : ion.Game) {
         super(game);
-        this.pos = Ion.point(game.canvas.width/2, game.canvas.height/2);
+        this.pos = ion.point(game.canvas.width/2, game.canvas.height/2);
     }
 }
 
